87118bed9e1a19c499499b8b186fdb857baf84be
shankagr7805 <iamshank7805@gmail.com>
2026-05-25 22:15:19 +0530

    fixed

--- src/main/java/com/fintrack/dto/response/Responses.java
+++ src/main/java/com/fintrack/dto/response/Responses.java
@@ -1,6 +1,5 @@
 package com.fintrack.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fintrack.entity.TransactionType;
 import lombok.AllArgsConstructor;
